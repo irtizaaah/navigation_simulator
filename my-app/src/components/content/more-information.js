f938968fcfdff85c279366f3bc698dc69c754cca
irtizaaah <irtizaaah@gmail.com>
2022-01-18 01:51:24 +0400

    Updated new line method  with <br/> tag

--- my-app/src/components/content/more-information.js
+++ my-app/src/components/content/more-information.js
@@ -3,10 +3,10 @@ import "./document-formatting.css"
 function MoreInformation(props){
   return (
     <div className = "tutorial_container">
-        <p className = "new_line"></p>
-        <p className = "new_line"></p>
+        <p><br/></p>
+        <p><br/></p>
         <p>Every square in this grid is represented by a node in a graph. Every node has an edge with a weight connecting it to it's adjacent nodes. This weight represents the cost of traveling from one node to another. </p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p>As mentioned above, this grid is represented by a graph under the hood. A graph traversal algorithm, like Dijkstra’s algorithm, can find the shortest path to every node. It does so by comparing the cumulative edge weights incurred by travelling every possible path from the starting node to every other node in order to find the path with the smallest cumulative weight. The order in which the algorithm visits these nodes is what is being visualized on the screen when you click ‘shortest path’.</p>
         <p className = "new_line"></p>
         <p>Here are the costs associated with traveling from square to square in this grid:</p>
--- my-app/src/components/content/tutorial.js
+++ my-app/src/components/content/tutorial.js
@@ -3,37 +3,37 @@ import "./document-formatting.css"
 function Tutorial(props){
   return (
     <div className = "tutorial_container">
-        <p className = "new_line"></p>
-        <p className = "new_line"></p>
+        <p><br/></p>
+        <p><br/></p>
         <p>This program will find the shortest route (by time) between any two locations in this grid -taking both distance and traffic into consideration. It's a very simplified version of how real navigation services are able to suggest you the fastest routes to your destinations.</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p className = "heading">Step 1: Pick a Map</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p>These "maps" are really just mazes, which you can customize.</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p>You can either pick pre-built ones based on real cities, or pick "none" or "filled" to build your own from scratch.</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p className = "heading">Step 2: Select Your Route</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p>Decide where you want to start and end your journey.</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p>You can either click on the "From" and "To" buttons, or directly the points on the grid you want to select.</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p>After selecting your points, click on the new sqaure you want to place your points.</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p className = "heading">Step 3: Build In Additional Items</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p>Customize your roads by adding and removing blocks, or by inserting traffic/congestion at any spot.</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p>Click on "block", "remove", or "traffic" depending on which edit mode you want to enter.</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p>After entering the "traffic" edit mode, click the square you want to insert traffic in.</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p>After entering "block" or "remove" edit modes, click any square to activate the brush, hover over the desired squares to draw in them, and click any square again to deactivate the brush.</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
         <p className = "heading">Step 4: Travel!</p><p className = "new_line"></p>
         <p>Once you're happy with your map, you can click "Quickest path" to watch the algorithm visually figure out the fastest route to your destination.</p>
-        <p className = "new_line"></p>
+        <p><br/></p>
     </div>
   );
 }
